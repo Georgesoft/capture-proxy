c5c27b423a4142b0349de278ca321ae8bd7fe8e9
Daniil Dubin <daniil.dubin@georgesoft.com>
2017-12-07 15:11:44 +0200

    Cache on request loading from fs

--- src/save.js
+++ src/save.js
@@ -5,12 +5,26 @@ var fsUtil = require('./util/fs');
 
 var saveFolder = path.join(fsUtil.homePath(), '.capture/save/');
 
+var cache = {};
+
 function save (filePath, name) {
     fsUtil.ensurePath(saveFolder);
     fs.writeFileSync(path.join(saveFolder, name), fs.readFileSync(filePath));
 }
 
 function load (name) {
+    var res = cache[name];
+    if (res) {
+        return res;
+    } else {
+        res = load0(name);
+        cache[name] = res;
+    }
+
+    return res;
+}
+
+function load0 (name) {
     var fileName = name;
     if (fs.existsSync(fileName)) {
         return fs.readFileSync(fileName, 'utf-8');
